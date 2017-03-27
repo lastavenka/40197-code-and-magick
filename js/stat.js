5b281acb4e137f934d9ea3349c59e682a1629981
Lastavenka <natasha.lastavenka@gmail.com>
2017-03-27 22:32:34 +0300

    Исправления в задании второго раздела по требованиям Travis

--- js/stat.js
+++ js/stat.js
@@ -1,49 +1,47 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.fillRect(110, 20, 420, 270);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(100, 10, 420, 270);
-    ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(100, 10, 420, 270);
+  ctx.strokeRect(100, 10, 420, 270);
 
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+  var max = -1;
+
+  for (var k = 0; k < times.length; k++) {
+    var time = times[k];
+      
+    if (time > max) {
+        max = time;
+      }
+  };
+
+  var histogramHeight = 150;
+  var step = histogramHeight / (max - 0);
+  var barWidth = 40;
+  var indent = barWidth + 50;
+  var initialX = 140;
+  var initialY = 260;
+
+  ctx.textBaseline = 'botttom';
+
+  for (var i = 0; i < times.length; i++) {
+    ctx.fillText(names[i], initialX + indent * i, initialY);
+
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255,' + Math.random() + ')';
+    }
+
+    ctx.fillRect(initialX + indent * i, initialY - 20 - times[i] * step, barWidth, times[i] * step);
     ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
-
-    ctx.fillText('Ура вы победили!', 120, 40);
-    ctx.fillText('Список результатов:', 120, 60);
-
-    var max = -1;
-    var maxIndex = -1;
-
-    for (var i = 0; i < times.length; i++) {
-        var time = times[i];
-        if (time > max) {
-            max = time;
-            maxIndex = i;
-        }
-    }
-
-    var histogramHeight = 150;
-    var step = histogramHeight / (max - 0);
-    var barWidth = 40;
-    var indent = barWidth + 50;
-    var initialX = 140;
-    var initialY = 260;
-
-    ctx.textBaseline = 'botttom';
-
-    for (var i = 0; i < times.length; i++) {
-        ctx.fillText(names[i], initialX + indent * i, initialY);
-
-        if (names[i] == 'Вы') {
-            ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-        } else {
-            ctx.fillStyle = 'rgba(0, 0, 255,' + Math.random() + ')';
-        };
-
-        ctx.fillRect(initialX + indent * i, initialY - 20 - times[i] * step, barWidth, times[i] * step);
-        ctx.fillStyle = '#000';
-        ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - 30 - times[i] * step);
-    }
+    ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - 30 - times[i] * step);
+  }
 };
